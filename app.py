e21d546bca32e3c9c24635bfd5663afaab90c3aa
DerickMacbookAir <derickdev6@gmail.com>
2022-05-22 15:52:57 -0500

    Implemente la url de actualizar empleado similar a la de nuevo empleado, seleccionando el empleado de una lista

--- app.py
+++ app.py
@@ -40,15 +40,37 @@ def nuevoempleado():
             charges_list.append(new_charge)
         return render_template('nuevoempleado.html', charges_list=charges_list)
     elif request.method == 'POST':
-        # next_id = int(
-        #     dbf.read(
-        #         """SELECT idEmpleado FROM empleados ORDER BY idEmpleado desc LIMIT 1""",
-        #         1)[0]) + 1
         new_employee = Employee(0, request.form['nombre'],
                                 int(request.form['cargo']),
                                 request.form['correo'])
-        print(str(new_employee))
+        # print(str(new_employee))
         dbf.create(
             f"""INSERT INTO empleados (idCargo, nombre, correo) VALUES({new_employee.charge}, "{new_employee.name}", "{new_employee.email}")"""
         )
         return redirect(url_for('nuevoempleado'))
+
+
+@app.route('/actualizarempleado', methods=['GET', 'POST'])
+def actualizarempleado():
+    if request.method == 'GET':
+        charges_list = []
+        query = dbf.read("""SELECT * FROM cargos""")
+        for item in query:
+            new_charge = Charge(item[0], item[1])
+            charges_list.append(new_charge)
+        id_list = dbf.read(
+            """SELECT idEmpleado, nombre, c.descripcion FROM empleados e inner join cargos c on e.idCargo = c.idCargo ORDER BY nombre"""
+        )
+        # id_list = dbf.read("""SELECT idEmpleado FROM empleados""")
+        return render_template('actualizarempleado.html',
+                               charges_list=charges_list,
+                               id_list=id_list)
+    elif request.method == 'POST':
+        updt_employee = Employee(int(request.form['idEmpleado']),
+                                 request.form['nombre'],
+                                 int(request.form['cargo']),
+                                 request.form['correo'])
+        dbf.update(
+            f"""UPDATE empleados SET nombre = '{updt_employee.name}', idCargo = {updt_employee.charge}, correo = '{updt_employee.email}' WHERE idEmpleado = {updt_employee.id}"""
+        )
+        return redirect(url_for('actualizarempleado'))
