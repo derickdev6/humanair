3da9a6208a6f4e6ebbf6e0e5fb96d0d785f7946a
DerickMacbookAir <derickdev6@gmail.com>
2022-05-22 01:28:17 -0500

    Lista de empleados ahora es ordenada por nombre, Funcion nuevo empleado implementada con GET y POST

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Imports para el desarrollo de la app
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, redirect, render_template, request, url_for
 from models import Charge, Employee
 import dbfunctions as dbf
 # Entry point
@@ -21,10 +21,34 @@ def home():
 def empleados():
     employee_list = []
     query = dbf.read(
-        """SELECT idEmpleado, nombre, c.descripcion, correo FROM empleados e inner join cargos c on e.idCargo = c.idCargo """
+        """SELECT idEmpleado, nombre, c.descripcion, correo FROM empleados e inner join cargos c on e.idCargo = c.idCargo ORDER BY nombre"""
     )
     for item in query:
         new_emp = Employee(item[0], item[1], item[2], item[3])
         employee_list.append(new_emp)
 
     return render_template('empleados.html', employee_list=employee_list)
+
+
+@app.route('/nuevoempleado', methods=['GET', 'POST'])
+def nuevoempleado():
+    if request.method == 'GET':
+        charges_list = []
+        query = dbf.read("""SELECT * FROM cargos""")
+        for item in query:
+            new_charge = Charge(item[0], item[1])
+            charges_list.append(new_charge)
+        return render_template('nuevoempleado.html', charges_list=charges_list)
+    elif request.method == 'POST':
+        next_id = int(
+            dbf.read(
+                """SELECT idEmpleado FROM empleados ORDER BY idEmpleado desc LIMIT 1""",
+                1)[0]) + 1
+        new_employee = Employee(next_id, request.form['nombre'],
+                                int(request.form['cargo']),
+                                request.form['correo'])
+        print(str(new_employee))
+        dbf.create(
+            f"""INSERT INTO empleados (idEmpleado, idCargo, nombre, correo) VALUES({next_id}, {new_employee.charge}, "{new_employee.name}", "{new_employee.email}")"""
+        )
+        return redirect(url_for('nuevoempleado'))
