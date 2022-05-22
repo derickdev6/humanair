7307f626327b11003c766d4b7e912ba7f92b7da2
DerickMacbookAir <derickdev6@gmail.com>
2022-05-22 01:27:13 -0500

    Funcion create implementada

--- dbfunctions.py
+++ dbfunctions.py
@@ -1,8 +1,17 @@
 import sqlite3
 
 
-def create():
-    pass
+def create(sql_sentence):
+    # Connects to DB
+    sqliteConnection = sqlite3.connect('humanairDB.sqlite')
+    cursor = sqliteConnection.cursor()
+    # Print statement and execution
+    cursor.execute(sql_sentence)
+    sqliteConnection.commit()
+    if cursor.rowcount > 0:
+        print(f'succesfully created {cursor.rowcount} items')
+    # cursor.close()
+    return True if cursor.rowcount > 0 else False
 
 
 def read(sql_sentence, *single):
@@ -13,10 +22,12 @@ def read(sql_sentence, *single):
     if single:
         print("Single")
         cursor.execute(sql_sentence)
+        # cursor.close()
         return cursor.fetchone()
     else:
         print("Multiple")
         cursor.execute(sql_sentence)
+        # cursor.close()
         return cursor.fetchall()
 
 
